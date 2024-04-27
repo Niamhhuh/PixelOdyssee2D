c2fe82d3bf7b5109038a61328b4f6d824f021afb
Vivid-Hat <dkasper@stud.macromedia.de>
2024-04-27 02:42:40 +0200

    Frogger addition
    added movement of obstacles and platforms. Frogger can stay on platforms. added border so no out of space. added collision --> death

--- 2D Pixel Odyssee/Assets/Scripts Frogger/Frogger.cs
+++ 2D Pixel Odyssee/Assets/Scripts Frogger/Frogger.cs
@@ -1,13 +1,25 @@
 using System.Collections;
+using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class Frogger : MonoBehaviour
+
 {
+    private SpriteRenderer spriteRenderer;
+    public Sprite idleSprite;
+    public Sprite leapSprite;
+    public Sprite deadSprite;
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    public float scrollSpeed = 3.0f;
     private void Update()
     {
 
         if (Input.GetKeyDown(KeyCode.W)) {
-            transform.rotation = Quaternion.Euler(0f,0f, 0f);
+            transform.rotation = Quaternion.Euler(0f , 0f, 0f);
             Move(Vector3.up);
         }
 
@@ -23,15 +35,49 @@ public class Frogger : MonoBehaviour
             transform.rotation = Quaternion.Euler(0f, 0f, -90f);
             Move(Vector3.right);
         }
+        else if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            transform.rotation = Quaternion.Euler(0f, 0f, -90f);
+            Move(Vector3.right);
+        }
+        else if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            transform.rotation = Quaternion.Euler(0f, 0f, 90f);
+            Move(Vector3.left);
+        }
+        float scrollInput = Input.GetAxis("Mouse ScrollWheel");
+        Vector3 newPosition = transform.position + Vector3.up * scrollInput;
+        transform.position = newPosition;
     }
 
     private void Move(Vector3 direction)
     {
-        //transform.position += direction;
-        Vector3 destination = transform.position + direction;
-        
-        StartCoroutine(Leap(destination));
 
+        Vector3 destination = transform.position + direction;
+
+        Collider2D barrier = Physics2D.OverlapBox(destination, Vector2.zero, 0f, LayerMask.GetMask("Barrier"));
+        Collider2D platform = Physics2D.OverlapBox(destination, Vector2.zero, 0f, LayerMask.GetMask("Platform"));
+        Collider2D obstacle = Physics2D.OverlapBox(destination, Vector2.zero, 0f, LayerMask.GetMask("Obstacle"));
+
+        if (barrier != null) {
+            return;
+        }
+        if (platform != null)
+        {
+            transform.SetParent(platform.transform);}
+            else
+            {
+            transform.SetParent(null);
+            }
+        if (obstacle != null && platform == null)
+        {
+            transform.position = destination;
+            Death();
+        }
+        else
+        {
+            StartCoroutine(Leap(destination));
+        }
     }
 
     private IEnumerator Leap(Vector3 destination)
@@ -41,6 +87,8 @@ public class Frogger : MonoBehaviour
         float elapsed = 0f;
         float duration = 0.125f;
 
+        //spriteRenderer.sprite = leapSprite;
+
         while (elapsed < duration)
         {
             float t = elapsed / duration;
@@ -49,6 +97,23 @@ public class Frogger : MonoBehaviour
             yield return null;
         }
 
-        transform.position = destination;  
+        transform.position = destination;
+        //spriteRenderer.sprite = idleSprite;
+    }
+
+    private void Death()
+    {
+
+        transform.rotation = Quaternion.identity;
+        spriteRenderer.sprite = deadSprite;
+        enabled = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (enabled && other.gameObject.layer == LayerMask.NameToLayer("Obstacle") && transform.parent == null)
+        {
+            Death();    
+        }
     }
 }
