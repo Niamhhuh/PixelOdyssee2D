e0425359db07e832337aff9a84e178465d6c5700
Niamhhuh <niamh.barreto@outlook.de>
2024-05-25 14:54:21 +0200

    AdvancedDialogueStart

---
+++ 2D Pixel Odyssee/Assets/DialogueFolder/DialogueScipts/AdvancedDialogueManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdvancedDialogueManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+public enum DialogueActors
+{
+    Rosie,
+    Bebe,
+    Silver,
+    Random,
+    Branch,
+};
