17d953c4236a692d2ca915b78aef2bde901de2d5
Niamhhuh <niamh.barreto@outlook.de>
2024-05-30 15:58:40 +0200

    WorkingDialogueUpToOptions

--- 2D Pixel Odyssee/Assets/DialogueFolder/DialogueScipts/AdvancedDialogueManager.cs
+++ 2D Pixel Odyssee/Assets/DialogueFolder/DialogueScipts/AdvancedDialogueManager.cs
@@ -1,19 +1,69 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AdvancedDialogueManager : MonoBehaviour
 {
+    //NPC DIALOGUE we are currently stepping through
+    private AdvancedDialogueSO currentConversation;
+    private int stepNum;
+    private bool dialogueActivated;
+
+    //UI REFERENCES
+    private GameObject dialogueCanvas;
+    private TMP_Text actor;
+    private Image portrait;
+    private TMP_Text dialogueText;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        dialogueCanvas = GameObject.Find("DialogueCanvas");
+        actor = GameObject.Find("ActorText").GetComponent<TMP_Text>();
+        portrait = GameObject.Find("PortraitImage").GetComponent<Image>();
+        dialogueText = GameObject.Find("DialogueText").GetComponent <TMP_Text>();
+
+        dialogueCanvas.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(dialogueActivated && Input.GetButtonDown("Interact"))
+        {
+            //Cancel dialogue if there are no lines of dialogue remaining
+            if (stepNum >= currentConversation.actors.Length)
+                TurnOffDialogue();
+
+            //Continue dialogue
+            else
+            {
+                dialogueText.text = currentConversation.dialogue[stepNum];
+                dialogueCanvas.SetActive(true);
+                stepNum += 1;
+            }
+               
+        }
+    }
+
+    public void InitiateDialogue(NPCDialogue npcDialogue)
+    {
+        //the array we are currently stepping through
+        currentConversation = npcDialogue.conversation[0];
+        Debug.Log("Started converstation" + currentConversation);
+
+        dialogueActivated = true;
+    }
+
+    public void TurnOffDialogue()
+    {
+        stepNum = 0;
+        Debug.Log("Ended converstation. Reset the step to" + stepNum);
+
+        dialogueActivated = false;
+        dialogueCanvas.SetActive(false);
     }
 }
 
--- 2D Pixel Odyssee/Assets/DialogueFolder/DialogueScipts/NPCDialogue.cs
+++ 2D Pixel Odyssee/Assets/DialogueFolder/DialogueScipts/NPCDialogue.cs
@@ -1,25 +1,70 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Tilemaps;
 using UnityEngine;
 
 public class NPCDialogue : MonoBehaviour
 {
     public AdvancedDialogueSO[] conversation;
 
+
+    private Transform player;
+    private SpriteRenderer speechBubbleRenderer;
+
     private AdvancedDialogueManager advancedDialogueManager;
 
-    private bool dialogueInitiated;
+    private bool dialogueInitated;
 
 
     // Start is called before the first frame update
     void Start()
     {
         advancedDialogueManager = GameObject.Find("DialogueManager").GetComponent<AdvancedDialogueManager>();
+        //speechBubbleRenderer = GetComponent<SpriteRenderer>();
+        //speechBubbleRenderer.enabled = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerStay2D(Collider2D collision)
     {
-        
+        if (collision.gameObject.tag == "Player" && !dialogueInitated)
+        {
+            //Speech Bubble On
+            //speechBubbleRenderer.enabled=true;
+
+            //Find the player's transform
+            player = collision.gameObject.GetComponent<Transform>();
+
+            // Check to see where the player is, and turn toward them
+            if (player.position.x > transform.position.x && transform.parent.localScale.x < 0)
+            {
+                Flip();
+            }
+            else if (player.position.x < transform.position.x && transform.parent.localScale.x < 0)
+            {
+                Flip();
+            }
+
+            advancedDialogueManager.InitiateDialogue(this);
+            dialogueInitated = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            //Speech Bubble Off
+            //speechBubbleRenderer.enabled = false;
+
+            advancedDialogueManager.TurnOffDialogue();
+            dialogueInitated = false;
+        }
+    }
+
+    private void Flip()
+    {
+        Vector3 currentScale = transform.parent.localScale;
+        currentScale.x *= -1;
+        transform.parent.localScale = currentScale;
     }
 }
