74cc3be11d75ac682629916dfd8634374ff1676d
Vivid-Hat <dkasper@stud.macromedia.de>
2024-10-20 16:16:50 +0200

    UI Pointer works now

--- 2D Pixel Odyssee/Assets/GLOBAL SYSTEMS/UI Tracking Mouse/UIToMouse.cs
+++ 2D Pixel Odyssee/Assets/GLOBAL SYSTEMS/UI Tracking Mouse/UIToMouse.cs
@@ -1,21 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class UiToMouse: MonoBehaviour
+public class UiToMouse : MonoBehaviour
 {
     private RectTransform rectTransform;
+    public Transform player;
+    public float playerSpeed = 5f;
+    public Canvas canvas;
+    private Vector3 targetPosition;
+    private bool movePlayer = false;
     void Start()
     {
         rectTransform = GetComponent<RectTransform>();
+        targetPosition = player.position;
+
     }
 
     void Update()
     {
-        Vector3 mousePosition = Input.mousePosition;
 
-        Vector3 newPosition = new Vector3(mousePosition.x, rectTransform.position.y, rectTransform.position.z);
+        if (Input.GetMouseButtonDown(0))
+        {
 
-        rectTransform.position = newPosition;
+            Vector3 mousePosition = Input.mousePosition;
+
+            Vector3 worldPosition = Camera.main.ScreenToWorldPoint(new Vector3(mousePosition.x, mousePosition.y, Camera.main.nearClipPlane));
+
+            worldPosition.y = -3.5f;
+            worldPosition.z = 0f;
+
+            rectTransform.position = worldPosition;
+
+            targetPosition = new Vector3(worldPosition.x, player.position.y, player.position.z);
+
+            movePlayer = true;
+
+            GameObject.Find("MovePointer").GetComponent<Image>().enabled = true;
+        }
+
+        if (movePlayer)
+        {
+            player.position = Vector3.MoveTowards(player.position, targetPosition, playerSpeed * Time.deltaTime);
+
+            if (player.position == targetPosition)
+            {
+                movePlayer = false;
+                GameObject.Find("MovePointer").GetComponent<Image>().enabled = false;
+            }
+        }
+
     }
 }
