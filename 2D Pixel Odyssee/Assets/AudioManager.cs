7de51de040c55f2ac24ac7a43b693b7419116e94
justinkoelmel <justin.koelmel@gmx.net>
2024-10-25 18:55:30 +0200

    Musik Titelcreen & Museum implementiert

---
+++ 2D Pixel Odyssee/Assets/AudioManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMOD;
+
+public class AudioManager : MonoBehaviour
+{
+    /*
+    [SerializeField] EventReference TitelscreenTheme;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayTitelscreenTheme();
+    }
+
+    public void PlayTitelscreenTheme()
+    {
+        RuntimeManager.PlayOneShot(TitelscreenTheme);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    */
+}
