6fc8b326731ce3c70b0cbe85731745732bdd3459
Niamhhuh <niamh.barreto@outlook.de>
2024-11-12 23:14:51 +0100

    ScoreandLosePanel

--- 2D Pixel Odyssee/Assets/ARCADE_ASTREROIDS/Scripts/Asteroid_GameManager.cs
+++ 2D Pixel Odyssee/Assets/ARCADE_ASTREROIDS/Scripts/Asteroid_GameManager.cs
@@ -1,28 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Asteroid_GameManager : MonoBehaviour
 {
     public Asteroid_Player player;
     public ParticleSystem explosion;
+    public TextMeshProUGUI scoreText; // Use TextMeshProUGUI instead of Text
 
     public float respawnTime = 3.0f;
-    public float respawnInvulnerabiltyTime = 3.0f;
+    public float respawnInvulnerabilityTime = 3.0f;
 
     public int lives = 3;
     public int score = 0;
+
+    private void Start()
+    {
+        // Initialize the score display at the start of the game
+        UpdateScoreText();
+    }
 
     public void AsteroidDestroyed(Asteroid asteroid)
     {
         this.explosion.transform.position = asteroid.transform.position;
         this.explosion.Play();
 
-        if(asteroid.size < 1.75) 
+        if (asteroid.size < 1.75f)
         {
             this.score += 100;
         }
-        else if(asteroid.size < 2.0f)
+        else if (asteroid.size < 2.0f)
         {
             this.score += 50;
         }
@@ -30,6 +38,14 @@ public class Asteroid_GameManager : MonoBehaviour
         {
             this.score += 25;
         }
+
+        // Update the score display
+        UpdateScoreText();
+    }
+
+    private void UpdateScoreText()
+    {
+        scoreText.text = "Score: " + this.score;
     }
 
     public void PlayerDied()
@@ -38,7 +54,7 @@ public class Asteroid_GameManager : MonoBehaviour
         this.explosion.Play();
         this.lives--;
 
-        if(this.lives <= 0)
+        if (this.lives <= 0)
         {
             GameOver();
         }
@@ -48,13 +64,12 @@ public class Asteroid_GameManager : MonoBehaviour
         }
     }
 
-
     public void Respawn()
     {
         this.player.transform.position = Vector3.zero;
         this.player.gameObject.layer = LayerMask.NameToLayer("IgnoreCollisions");
         this.player.gameObject.SetActive(true);
-       Invoke(nameof(TurnOnCollisions), this.respawnInvulnerabiltyTime);
+        Invoke(nameof(TurnOnCollisions), this.respawnInvulnerabilityTime);
     }
 
     private void TurnOnCollisions()
@@ -64,6 +79,6 @@ public class Asteroid_GameManager : MonoBehaviour
 
     private void GameOver()
     {
-
+        Debug.Log("Game Over");
     }
 }
