c889d40a9a3d70e51d55c2d9dd4ed9cc4579fce5
Primordial <64266457+ConceptualGhost@users.noreply.github.com>
2024-11-13 10:51:59 +0100

    Fixed a Trigger Bug
    Added a null exception to Trigger Activation
    ...
    because I rather ignore errors than to fix them

--- 2D Pixel Odyssee/Assets/GLOBAL SYSTEMS/ProgressDataManager/DataManager.cs
+++ 2D Pixel Odyssee/Assets/GLOBAL SYSTEMS/ProgressDataManager/DataManager.cs
@@ -404,9 +404,10 @@ public class DataManager : MonoBehaviour
 
     public void TriggerActivate(int Target_ID)                                //Activate the Trigger
     {
+        print(TriggeredObjects_List.Count);
         foreach (GameObject TriggerObj in TriggeredObjects_List)
         {
-            if (TriggerObj.GetComponent<Triggerable>().ID == Target_ID)
+            if (TriggerObj != null && TriggerObj.GetComponent<Triggerable>().ID == Target_ID)
             {
                 if(TriggerObj.GetComponent<Triggerable>().Trigger_Passed != true)
                 TriggerObj.SetActive(true);
